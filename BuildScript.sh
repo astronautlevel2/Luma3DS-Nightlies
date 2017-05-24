eb6a01322e143536138e2a13cdc0e33407f0867b
Alex Taber <alexftaber@gmail.com>
2017-05-24 08:50:37 -0400

    Fix two things mentioned in https://github.com/astronautlevel2/Luma3DS/issues/7

--- BuildScript.sh
+++ BuildScript.sh
@@ -5,6 +5,8 @@ git clone git@github.com:AuroraWright/Luma3DS.git
 cd Luma3DS
 make
 zip "Luma3DS-${commit}.zip" out/boot.firm
+cp "Luma3DS-${commit}.zip" ../../Luma3DS-Site/latest.zip
+echo $commit > ../../Luma3DS-Site/lastCommit
 mv "Luma3DS-${commit}.zip" ../../Luma3DS-Site/builds
 cd ..
 rm -rf Luma3DS
